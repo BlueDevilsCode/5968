5dbe1552b9856a9fc392439136ed3012e62e3022
Scoutdrago3 <Drago7654321@gmail.com>
2016-12-10 19:40:50 -0500

    small changes

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThatHertzColorSensor.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThatHertzColorSensor.java
@@ -1,25 +1,38 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.DigitalChannelController;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by Sa'id on 12/10/2016.
  */
 public class ThatHertzColorSensor extends OpMode {
+    ElapsedTime runtime = new ElapsedTime();
+
+    private ColorSensor color = null;
+    private DeviceInterfaceModule dim = null;
+    private static final int LED_CHANNEL = 5;
 
     @Override
     public void init() {
+        color = hardwareMap.colorSensor.get("color");
+        dim = hardwareMap.deviceInterfaceModule.get("d_i_m");
 
+        dim.setDigitalChannelMode(LED_CHANNEL, DigitalChannelController.Mode.OUTPUT);
     }
 
     @Override
-    public void start() {
-
-    }
+    public void start() {runtime.reset();}
 
     @Override
     public void loop() {
-
+        telemetry.addData("ARGB", "" + color.argb());
+        telemetry.addData("Green", "" + color.green());
+        telemetry.addData("Blue", "" + color.blue());
+        telemetry.addData("Red", "" + color.red());
+        telemetry.addData("Clear/Alpha", "" + color.alpha());
     }
 }
