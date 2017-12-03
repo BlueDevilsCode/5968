51c78712e72c2f34e16b327ec056f1f1be32d63a
Sa'id <drago7654321@gmail.com>
2017-12-03 18:59:43 -0500

    updated app master, changed motor directions and servo type

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThatHertzTCTeleOp.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThatHertzTCTeleOp.java
@@ -1,19 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
- * Created by Sa'id on 10/14/2017.
+ * Created by westfield_robotics on 11/12/2017.
  */
 
-@TeleOp(name = "ThatHertzTeleOp", group = "Tests")
-public class ThatHertzTeleOp extends OpMode {
-
+public class ThatHertzTCTeleOp extends OpMode {
     private DcMotor frontRightMotor;
     private DcMotor frontLeftMotor;
     private DcMotor backRightMotor;
@@ -25,7 +21,7 @@ public class ThatHertzTeleOp extends OpMode {
     private DcMotor elbow;
     private Servo rightClaw;
     private Servo leftClaw;
-    private CRServo wrist;
+    private Servo wrist;
 
     @Override
     public void init() {
@@ -40,14 +36,14 @@ public class ThatHertzTeleOp extends OpMode {
         elbow = hardwareMap.dcMotor.get("elbow");
         rightClaw = hardwareMap.servo.get("rClaw");
         leftClaw = hardwareMap.servo.get("lClaw");
-        wrist = hardwareMap.crservo.get("wrist");
+        wrist = hardwareMap.servo.get("wrist");
 
         posDiagServos.setPosition(.5);
         negDiagServos.setPosition(.5);
 
         rightClaw.setPosition(.5);
         leftClaw.setPosition(.5);
-
+        wrist.setPosition(.5);
         frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -60,76 +56,67 @@ public class ThatHertzTeleOp extends OpMode {
             posDiagServos.setPosition(.5);
             negDiagServos.setPosition(.5);
 
-            frontRightMotor.setPower(-.5 * gamepad1.right_stick_y);
-            frontLeftMotor.setPower(-.5 * gamepad1.right_stick_y);
-            backRightMotor.setPower(-.5 * gamepad1.right_stick_y);
-            backLeftMotor.setPower(-.5 * gamepad1.right_stick_y);
+            frontRightMotor.setPower(.5 * gamepad1.right_stick_y -.1);
+            frontLeftMotor.setPower(.5 * gamepad1.right_stick_y -.1);
+            backRightMotor.setPower(.5 * gamepad1.right_stick_y-.1);
+            backLeftMotor.setPower(.5 * gamepad1.right_stick_y-.1);
         } else if (gamepad1.right_stick_y > .2) {
             posDiagServos.setPosition(.5);
             negDiagServos.setPosition(.5);
 
-            frontRightMotor.setPower(-.5 * gamepad1.right_stick_y);
-            frontLeftMotor.setPower(-.5 * gamepad1.right_stick_y);
-            backRightMotor.setPower(-.5 * gamepad1.right_stick_y);
-            backLeftMotor.setPower(-.5 * gamepad1.right_stick_y);
+            frontRightMotor.setPower(.5 * gamepad1.right_stick_y-.1);
+            frontLeftMotor.setPower(.5 * gamepad1.right_stick_y-.1);
+            backRightMotor.setPower(.5 * gamepad1.right_stick_y-.1);
+            backLeftMotor.setPower(.5 * gamepad1.right_stick_y-.1);
         } else if (gamepad1.right_stick_x > .2) {
-            posDiagServos.setPosition(.5 + (.5 * (2.0 / 3.0)));
-            negDiagServos.setPosition(.5 - (.5 * (2.0 / 3.0)));
+            posDiagServos.setPosition(.5 + (.5 * (2.0 / 3.0)) + 0.1);
+            negDiagServos.setPosition(.5 - (.5 * (2.0 / 3.0)) - 0.1);
 
-            frontRightMotor.setPower(-.5 * gamepad1.right_stick_x);
-            frontLeftMotor.setPower(.5 * gamepad1.right_stick_x);
-            backRightMotor.setPower(.5 * gamepad1.right_stick_x);
-            backLeftMotor.setPower(-.5 * gamepad1.right_stick_x);
+            frontRightMotor.setPower(-.5 * gamepad1.right_stick_x+.1);
+            frontLeftMotor.setPower(.5 * gamepad1.right_stick_x-.1);
+            backRightMotor.setPower(.5 * gamepad1.right_stick_x-.1);
+            backLeftMotor.setPower(-.5 * gamepad1.right_stick_x+.1);
         } else if (gamepad1.right_stick_x < -.2) {
-            posDiagServos.setPosition(.5 + (.5 * (2.0 / 3.0)));
-            negDiagServos.setPosition(.5 - (.5 * (2.0 / 3.0)));
+            posDiagServos.setPosition(.5 + (.5 * (2.0 / 3.0)) + 0.1);
+            negDiagServos.setPosition(.5 - (.5 * (2.0 / 3.0)) - 0.1);
 
-            frontRightMotor.setPower(-.5 * gamepad1.right_stick_x);
-            frontLeftMotor.setPower(.5 * gamepad1.right_stick_x);
-            backRightMotor.setPower(.5 * gamepad1.right_stick_x);
-            backLeftMotor.setPower(-.5 * gamepad1.right_stick_x);
+            frontRightMotor.setPower(-.5 * gamepad1.right_stick_x +.1);
+            frontLeftMotor.setPower(.5 * gamepad1.right_stick_x -.1);
+            backRightMotor.setPower(.5 * gamepad1.right_stick_x -.1);
+            backLeftMotor.setPower(-.5 * gamepad1.right_stick_x +.1);
         }
         else if (gamepad1.right_bumper){
-            posDiagServos.setPosition(.5);
-            negDiagServos.setPosition(.5);
-
-            frontRightMotor.setPower(-.5  * gamepad1.right_stick_x);
-            frontLeftMotor.setPower(.5 * gamepad1.right_stick_x);
-            backRightMotor.setPower(-.5 * gamepad1.right_stick_x);
-            backLeftMotor.setPower(.5 * gamepad1.right_stick_x);
+            frontRightMotor.setPower(-.5 * gamepad1.right_stick_x +.1);
+            frontLeftMotor.setPower(.5 * gamepad1.right_stick_x -.1);
+            backRightMotor.setPower(-.5 * gamepad1.right_stick_x +.1);
+            backLeftMotor.setPower(.5 * gamepad1.right_stick_x -.1);
         }
         else if (gamepad1.left_bumper){
-            posDiagServos.setPosition(.5);
-            negDiagServos.setPosition(.5);
-
-            frontRightMotor.setPower(.5 * gamepad1.right_stick_x);
-            frontLeftMotor.setPower(-.5 * gamepad1.right_stick_x);
-            backRightMotor.setPower(.5 * gamepad1.right_stick_x);
-            backLeftMotor.setPower(-.5 * gamepad1.right_stick_x);
-        } else {
+            frontRightMotor.setPower(.5 * gamepad1.right_stick_x -.1);
+            frontLeftMotor.setPower(-.5 * gamepad1.right_stick_x +.1);
+            backRightMotor.setPower(.5 * gamepad1.right_stick_x -.1);
+            backLeftMotor.setPower(-.5 * gamepad1.right_stick_x +.1);
+        }
+        else {
             frontRightMotor.setPower(0);
             frontLeftMotor.setPower(0);
             backRightMotor.setPower(0);
             backLeftMotor.setPower(0);
         }
 
-        if (gamepad1.left_trigger > 0) {
-            wrist.setPower(1);
+        if (gamepad2.right_stick_y > 0.2) {
+            wrist.setPosition(.0);
         }
-        else if (gamepad1.right_trigger > 0) {
-            wrist.setPower(-1);
+        else if (gamepad2.right_stick_y < -0.2){
+            wrist.setPosition(1);
         }
-        else {
-            wrist.setPower(0);
-        }
-
-        if (gamepad1.a) {
+        if (gamepad2.left_stick_x > 0.2){
             leftClaw.setPosition(1);
             rightClaw.setPosition(0);
-        } else if (gamepad1.b) {
+        } else if (gamepad2.left_stick_x < -0.2) {
             leftClaw.setPosition(.1);
             rightClaw.setPosition(.9);
-        } else if (gamepad1.y) {
+        } else{
             leftClaw.setPosition(.5);
             rightClaw.setPosition(.5);
         }
@@ -142,4 +129,5 @@ public class ThatHertzTeleOp extends OpMode {
             elbow.setPower(0);
         }
     }
+
 }
